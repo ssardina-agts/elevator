49ca426a4788b8736a8fe6c95d6a6f6da83f3e6d
andresjarami <jaramilloandres@hotmail.es>
2021-10-28 22:51:01 +1100

    fix some problems with variables

--- model/state.py
+++ model/state.py
@@ -10,25 +10,22 @@ class State(object):
     def __init__(self, people_number, floors_number, info_cars):
         people = []
         cars = []
-        cars_empty = True
-        all_people_arrived = False
         for idx in range(people_number):
-            person = Person(floors_number)
+            person = Person(floors_number=floors_number)
             # s_f = person.start_floor  # s_f is shorthand for start_floor to clean the code
 
             # floor_population[s_f] += 1
             people.append(person)
-        for idx in range(len(info_cars['car_number'])):
-            car = Car(capacity=info_cars['capacity'][idx])
+        for idx in range(info_cars['car_number']):
+            car = Car(capacity_value=info_cars['capacity'][idx])
             cars.append(car)
 
-        self.status = {"people": people, "cars": cars, "floors_number": floors_number, 'cars_empty': cars_empty,
-                       'all_people_arrived': all_people_arrived}
+        self._status = {"people": people, "cars": cars, "floors_number": floors_number, 'arrived_people_number': 0}
 
     @property
     def status(self):
-        return self.status
+        return self._status
 
     @status.setter
     def status(self, value: dict):
-        self.status = value
+        self._status = value
