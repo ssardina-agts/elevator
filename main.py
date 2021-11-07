6a21eddb3f60271af6f9c02709367ade8378e324
ssardina <ssardina@gmail.com>
2021-11-07 11:54:53 +1100

    Factored out params to CLI options FIxes #11

--- main.py
+++ main.py
@@ -1,17 +1,60 @@
-from controller.baseline_agents import Baseline, Efficient
-from model.simulator import Simulator
-import coloredlogs
+import argparse
 import logging
 
+import coloredlogs
 
-def main():
-    simulator = Simulator(people_number=10, floors_number=4, info_cars={"car_number": 1, "capacity": [1]})
+from controller.baseline_agents import Baseline, Efficient
+from model.simulator import Simulator
+
+def main(args):
+    simulator = Simulator(people_number=args.num_people, floors_number=args.num_floors, info_cars={"car_number": 1, "capacity": [3]})
     agent = Baseline()
     # agent = Efficient()
 
     simulator.register_agent(agent)
-    simulator.run(animation_speed=0.5)
+    simulator.run(animation_speed=args.anim_speed)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="""
+        An elevator simulator in Python based on https://github.com/mrbrianevans/elevator
+        """
+    )
+    # parser.add_argument(
+    #     dest='COBURG_SHEET',
+    #     type=str,
+    #     help='The Excel sheet Coburg sends with games.'
+    # )
+    parser.add_argument(
+        '--id',
+        help='Id if the simulation (if any).'
+    )
+    parser.add_argument(
+        '--num-people',
+        type=int,
+        default=10,
+        help='Do not set the RSVP option %(default)s.',
+    )
+    parser.add_argument(
+        '--num-floors',
+        type=int,
+        default=4,
+        help='Number of floor in building %(default)s.',
+    )
+    parser.add_argument(
+        '--anim-speed',
+        type=float,
+        default=0.5,
+        help='Delay step in the simulation, larger means slower simulation %(default)s.',
+    )
+    parser.add_argument(
+        '--gui',
+        action="store_true",
+        help='Show the GUI display.',
+    )
+    # we could also use vars(parser.parse_args()) to make args a dictionary args['<option>']
+    args = parser.parse_args()
+    print(args)
+
+    main(args)
